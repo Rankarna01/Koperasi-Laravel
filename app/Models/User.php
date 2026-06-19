2ef33ad6227273b551a943f1589eab6bc30cf291
MUHAMMAD RANDY KARNA <muhammadrandykarna@gmail.com>
2026-06-20 00:00:25 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Activitylog\Models\Concerns\LogsActivity;
-use Spatie\Activitylog\Support\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class User extends Authenticatable
 {
